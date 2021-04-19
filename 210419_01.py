155a5f66af6c30bb565f8213888f1e17f2896b1f
Jiyong Park <archymi77@gmail.com>
2021-04-19 23:41:08 +0900

    not answered

--- 210419_01.py
+++ 210419_01.py
@@ -14,14 +14,17 @@ def dfs(now_day, target_day, works, visited, result):
         answerList.append(result)
 
     for (idx, [during, money]) in enumerate(works):
-        if idx + during > target_day:
-            answerList.append(result)
-            break
         if (idx not in visited) and now_day <= idx:
             tmp = copy.deepcopy(visited)
             tmp.append(idx)
             dfs(idx + during, target_day, works, tmp, result + money)
+        if idx + during > target_day:
+            answerList.append(result)
+            break
 
 
 dfs(-1, day, work_list, list(), 0)
-print(max(answerList))
+if len(answerList) == 0:
+    print(0)
+else:
+    print(max(answerList))
