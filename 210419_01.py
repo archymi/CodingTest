696a9a18ae95e98aed2ef9b83182c0df94e237b0
Jiyong Park <archymi77@gmail.com>
2021-04-19 23:36:02 +0900

    not answered

---
+++ 210419_01.py
@@ -0,0 +1,27 @@
+# https://www.acmicpc.net/problem/14501
+import copy
+
+day = int(input())
+work_list = list()
+for i in range(day):
+    a, b = map(int, input().split())
+    work_list.append([a, b])
+
+answerList = list()
+
+def dfs(now_day, target_day, works, visited, result):
+    if now_day == target_day:
+        answerList.append(result)
+
+    for (idx, [during, money]) in enumerate(works):
+        if idx + during > target_day:
+            answerList.append(result)
+            break
+        if (idx not in visited) and now_day <= idx:
+            tmp = copy.deepcopy(visited)
+            tmp.append(idx)
+            dfs(idx + during, target_day, works, tmp, result + money)
+
+
+dfs(-1, day, work_list, list(), 0)
+print(max(answerList))
