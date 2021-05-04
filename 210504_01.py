b2b925529d93243496ef026c0114281e48820be7
archymi <archymi77@gmail.com>
2021-05-04 23:12:01 +0900

    not answered

--- 210504_01.py
+++ 210504_01.py
@@ -1,18 +1,23 @@
+# https://programmers.co.kr/learn/courses/30/lessons/49189
+import collections
+
 answerDict = dict()
 
-def findHeight(str_tree:dict, now:int, depth: int):
+
+def findHeight(str_tree: dict, now: int, depth: int):
     if now in str_tree:
         for a in str_tree[now]:
             if a not in answerDict:
                 answerDict[a] = depth + 1
+                print([a, depth + 1])
 
         for a in str_tree[now]:
             for b in str_tree[a]:
                 if b not in answerDict:
-                    findHeight(str_tree, a, depth + 1)
+                    findHeight(str_tree, a, answerDict[a])
+
 
 def solution(n, edge):
-    maxVal = 0
     count = 0
     str_tree = dict()
 
@@ -32,15 +37,15 @@ def solution(n, edge):
     answerDict[1] = 0
     findHeight(str_tree, 1, 0)
 
-    print(answerDict)
+    # print(answerDict)
 
-    for a in answerDict.keys():
-        if answerDict[a] > maxVal:
-            maxVal = answerDict[a]
+    maxVal = max(answerDict.values())
 
     for a in answerDict.keys():
         if answerDict[a] == maxVal:
             count = count + 1
     return count
 
-print(solution(6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]))
+
+#print(solution(6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]))
+print(solution(1, [[1, 2]]))
