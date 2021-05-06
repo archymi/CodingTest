83c54635d746ed6c192ab4e503ccfb4322dd3e93
jiyong_park <jiyong_park@tmax.co.kr>
2021-05-06 20:13:26 +0900

    answered

--- 210506_01.py
+++ 210506_01.py
@@ -3,18 +3,18 @@ def solution(new_id):
     def check_1(new: str):
         return new.lower()
 
-    # new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+    # new_id 에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
     def check_2(new: str):
         new_str: str = ''
         for c in new:
             if (('a' <= c and c <= 'z') or
-               ('1' <= c and c <= '9') or
+               ('0' <= c and c <= '9') or
                ('-' == c) or ('_' == c) or
                ('.' == c)):
                 new_str += c
         return new_str
 
-    # new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+    # new_id 에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
     def check_3(new: str):
         new_str: str = str('')
         if len(new) == 0:
@@ -29,7 +29,7 @@ def solution(new_id):
                     new_str += c
         return new_str
 
-    # new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+    # new_id 에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
     def check_4(new: str):
         new_str: str = str('')
         if len(new) == 0:
@@ -57,19 +57,21 @@ def solution(new_id):
         else:
             return new
 
-    # new_id
+    # new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+    # 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
     def check_6(new: str):
-        new_str:str = ''
+        if new == '':
+            return ''
+        new_str: str = ''
         if len(new) >= 16:
             new_str = new[:15]
         else:
             new_str = new
-        if len(new_str) >= 2 and new_str[-1] == '.':
+        if len(new_str) != 0 and new_str[-1] == '.':
             new_str = new_str[:-1]
-        elif len(new_str) == 1 and new_str[0] == '.':
-            return ''
         return new_str
 
+    # new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
     def check_7(new: str):
         new_str:str = new
         if len(new_str) <= 2:
@@ -96,5 +98,5 @@ def solution(new_id):
 #solution("...!@BaT#*..y.abcdefghijklm")
 #solution("z-+.^.")
 #solution("=.=")
-solution("123_.def")
+solution(".a.a.a.a.B.a.a.")
 #solution("")
